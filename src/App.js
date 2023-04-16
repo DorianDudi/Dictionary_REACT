4b5adacaf4d86dd9d1dd3d656cfe2cea7fdac2b5
Dorian-DV <ddorianvlad@yahoo.ro>
2023-04-16 15:56:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ export default function App() {
     <>
     <div>
         <form>
-          <input type="text" id="addField" placeholder="Type word to add" />
-          <button type="button" id="addTextButton" onClick={() => addWord(dictionary)}>Add to dictionary...</button>
+          <input type="text" id="addField" placeholder="Type word to add..." />
+          <button type="button" id="addTextButton" onClick={() => addWord(dictionary)}>Add to dictionary</button>
         </form>
         <form>
-          <input type="text" id="searchField" placeholder="Type word to search for" />
-          <button type="button" id="searchButton" onClick={() => searchForWord(dictionary)}>Search dictionary...</button>
+          <input type="text" id="searchField" placeholder="Type word to search for..." />
+          <button type="button" id="searchButton" onClick={() => searchForWord(dictionary)}>Search dictionary</button>
         </form>
     </div>
     <div id="userMessage"></div>
