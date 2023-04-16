ac64353dd3842e42667f2211b8195c68c1d899fa
Dorian-DV <ddorianvlad@yahoo.ro>
2023-04-16 15:32:50 +0300

    Update addWord.js

--- src/dictionar_app_src/addWord.js
+++ src/dictionar_app_src/addWord.js
@@ -1,10 +1,15 @@
 export default function addWord(dictionary_param) {
     let inputField = document.getElementById("addField");
+	const userMessage = document.getElementById("userMessage");
 	if(inputField.value != "") {
 		dictionary_param.push(inputField.value);
-		alert(inputField.value + " has been added to dictionary");
+		//alert(inputField.value + " has been added to dictionary");
 		inputField.value = "";
+		userMessage.innerHTML = "<h3>Word added to dictionary!</h3>";
+		setTimeout(() => {userMessage.innerHTML = "";}, 4000);
 	} else {
-		alert("Please type the word you want to add");
+		//alert("Please type the word you want to add");
+		userMessage.innerHTML = "<h3>Please type the word you want to add</h3>";
+		setTimeout(() => {userMessage.innerHTML = "";}, 4000);
 	}
 }
