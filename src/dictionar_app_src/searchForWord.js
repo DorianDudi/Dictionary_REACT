3b1971b718cd88e946be2a65df2dd9fe84228a8e
Dorian-DV <ddorianvlad@yahoo.ro>
2023-04-16 15:33:41 +0300

    Update searchForWord.js

--- src/dictionar_app_src/searchForWord.js
+++ src/dictionar_app_src/searchForWord.js
@@ -1,9 +1,13 @@
 export default function searchForWord(words_array) {
 	let searchField = document.getElementById("searchField");
+	const userMessage = document.getElementById("userMessage");
 	if(words_array.includes(searchField.value)) {
-		alert("Word found in Dictionary!");
+		userMessage.innerHTML = "<h3>Word found in Dictionary!</h3>";
+		setTimeout(() => {userMessage.innerHTML = "";}, 4000);
 	} else {
-		alert("Word not found in Dictionary!");
+		userMessage.innerHTML = "<h3>Word not found in Dictionary!</h3>";
+		setTimeout(() => {userMessage.innerHTML = "";}, 4000);
 	}
 	searchField.value = "";
 }
+
