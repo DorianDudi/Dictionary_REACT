e00ffef80f2380d75487edd0cfa7ddfb94d15532
Dorian-DV <ddorianvlad@yahoo.ro>
2023-04-16 18:39:32 +0300

    Update searchForWord.js

--- src/dictionar_app_src/searchForWord.js
+++ src/dictionar_app_src/searchForWord.js
@@ -1,6 +1,6 @@
 export default function searchForWord(words_array) {
 	let searchField = document.getElementById("searchField");
-	const userMessage = document.getElementById("userMessage");
+	let userMessage = document.getElementById("userMessage");
 	if(words_array.includes(searchField.value)) {
 		userMessage.innerHTML = "<h3>Word found in Dictionary!</h3>";
 		setTimeout(() => {userMessage.innerHTML = "";}, 4000);
